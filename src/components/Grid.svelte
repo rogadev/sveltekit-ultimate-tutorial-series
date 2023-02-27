ef2af05e4be313a487838cf50b2c70fc83d6e08e
Roga <61405141+rogadev@users.noreply.github.com>
2023-02-27 13:01:29 -0800

    Update Grid.svelte
    Leaving line 13 as-was, users get the error "Block-scoped variable 'blok' used before its declaration.ts (2448)". Alternatively, you could do:
    
    ```svelte
    {#each columns as b}
      <div class="flex-auto px-6">
        <StoryblokComponent {blok: b} />
      </div>
    {/each}
    ```
    
    Or similar variable naming that you feel better conveys a clearer picture of what's happening.
    
    > Note: `Page.svelte` is also affected by this issue but was not updated in this proposal.

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,16 +1,18 @@
 <script>
-  import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
+	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
 
-  export let blok;
+	export let blok;
+
+  $: columns = blok.columns;
 </script>
 
 <div
-  use:storyblokEditable={blok}
-  class="container mx-auto grid md:grid-cols-3 gap-16 place-items-center"
+	use:storyblokEditable={blok}
+	class="container mx-auto grid md:grid-cols-3 gap-16 place-items-center"
 >
-  {#each blok.columns as blok}
-    <div class="flex-auto px-6">
-      <StoryblokComponent {blok} />
-    </div>
-  {/each}
+	{#each columns as blok}
+		<div class="flex-auto px-6">
+			<StoryblokComponent {blok} />
+		</div>
+	{/each}
 </div>
